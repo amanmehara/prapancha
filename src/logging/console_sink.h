e14ffd95957276b5e1695f8ed82aefc510ca7a86
amanmehara <amanmehara@zohomail.in>
2026-02-27 02:57:38 +0530

    feat(logging): 🖥 add runtime toggle and noexcept safety to ConsoleSink

--- src/logging/console_sink.h
+++ src/logging/console_sink.h
@@ -15,10 +15,18 @@
 namespace mehara::prapancha::logging {
     class ConsoleSink : public LogSink<ConsoleSink> {
     public:
-        void write(LogLevel level, std::string_view msg) const {
+        explicit ConsoleSink(bool enabled = true) noexcept : enabled_(enabled) {}
+
+        void write(LogLevel level, std::string_view msg) const noexcept {
+            if (!enabled_) {
+                return;
+            }
             const auto &color = get_traits(level).color;
             std::println(stdout, "{}{}{}", color, msg, LogLevelTraits::reset);
         }
+
+    private:
+        bool enabled_;
     };
 } // namespace mehara::prapancha::logging
 
